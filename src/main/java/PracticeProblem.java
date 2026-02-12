cb88f94ba2305883abc2423fc6386705c09d5262
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-02-12 16:12:30 +0000

    Buh

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,13 @@
+/**
+ * File Name: practice problem 1.3 - Strings
+* Author: Alfred Yang
+* Date Created: Feb 12, 2026
+* Date Last Modified: Feb 12, 2026
+ */
+
+
 public class PracticeProblem {
+	
 
 	public static void main(String args[]) {
 		q1();
@@ -9,18 +18,31 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String cow = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+		System.out.println(cow);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String Fortnite = "Fortnite ";
+		String isAwful = "is awful";
+		System.out.print(Fortnite);
+		System.out.println(isAwful);
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String dont = "Don't "; 
+		String a = "@ me";
+		System.out.println(dont + a);
 	}
 
 
